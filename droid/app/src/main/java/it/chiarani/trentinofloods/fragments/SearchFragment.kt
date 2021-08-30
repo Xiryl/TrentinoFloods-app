1426143fbc0bcbe7ce5bb956fc6d97e102e5297f
Fabio <fabio@chiarani.it>
2021-08-30 22:17:43 +0200

    add new fragment

--- droid/app/src/main/java/it/chiarani/trentinofloods/fragments/SearchFragment.kt
+++ droid/app/src/main/java/it/chiarani/trentinofloods/fragments/SearchFragment.kt
@@ -3,20 +3,16 @@ package it.chiarani.trentinofloods.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import it.chiarani.trentinofloods.R
 import it.chiarani.trentinofloods.databinding.FragmentHomeBinding
+import it.chiarani.trentinofloods.databinding.FragmentSearchBinding
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class SearchFragment : Fragment(R.layout.fragment_search) {
 
-    private lateinit var binding: FragmentHomeBinding
+    private lateinit var binding: FragmentSearchBinding
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding = FragmentHomeBinding.bind(view)
-
-        binding.clCardSearch.setOnClickListener { view ->
-            view.findNavController().navigate(R.id.action_homefragment_to_searchfragment)
-         }
+        binding = FragmentSearchBinding.bind(view)
     }
 }
