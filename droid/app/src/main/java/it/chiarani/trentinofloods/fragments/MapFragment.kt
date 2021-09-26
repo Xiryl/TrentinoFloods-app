72d837dde286956ae3b0d6da196d75969873416e
Fabio <fabio@chiarani.it>
2021-09-26 17:27:15 +0200

    add marker on maps

--- droid/app/src/main/java/it/chiarani/trentinofloods/fragments/MapFragment.kt
+++ droid/app/src/main/java/it/chiarani/trentinofloods/fragments/MapFragment.kt
@@ -13,10 +13,13 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.gson.Gson
 import it.chiarani.trentinofloods.R
 import it.chiarani.trentinofloods.adapters.FloodsDataAdapter
+import it.chiarani.trentinofloods.data.SensorData
 import it.chiarani.trentinofloods.databinding.FragmentFavoriteBinding
 import it.chiarani.trentinofloods.databinding.FragmentMapBinding
 import it.chiarani.trentinofloods.utils.Constants
@@ -56,17 +59,40 @@ class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback {
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
-        //46.0374189,11.0243494 tn
-
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(
-            MarkerOptions()
-            .position(sydney)
-            .title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val trento = LatLng(46.0374189, 11.0243494)
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(trento))
+
+        val floodList = getJsonFloods()
+
+        for(flood in floodList.features) {
+            if(flood.properties.strumento == "Idrometro") {
+                mMap.addMarker(
+                    MarkerOptions()
+                        .position(LatLng(flood.geometry.coordinates[1], flood.geometry.coordinates[0]))
+                        .title(flood.properties.bacino + " - " + flood.properties.stazione)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)))
+            } else {
+                mMap.addMarker(
+                    MarkerOptions()
+                        .position(LatLng(flood.geometry.coordinates[1], flood.geometry.coordinates[0]))
+                        .title(flood.properties.bacino + " - " + flood.properties.stazione)
+                        .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA)))
+            }
+
+        }
+    }
+
+
+    fun getJsonFloods() : SensorData {
+        val fileInString: String = requireContext().assets.open("Pluviometers.json").bufferedReader().use { it.readText() }
+        val fileIdroInString: String = requireContext().assets.open("Idrometers.json").bufferedReader().use { it.readText() }
+        val pluviometerData = Gson().fromJson(fileInString, SensorData::class.java)
+        val idrometerData = Gson().fromJson(fileIdroInString, SensorData::class.java)
+        idrometerData.features.addAll(pluviometerData.features)
+        return idrometerData
     }
 
 }
